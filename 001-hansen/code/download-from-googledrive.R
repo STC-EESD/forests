4600881e671ca84e11bacfc03dea65c324b4ad16
Kenneth Chu <kennethchu@Kenneths-MacBook-Pro.local>
2022-12-31 13:07:16 -0500

    updated: code/download-from-googledrive.R

--- 001-hansen/code/download-from-googledrive.R
+++ 001-hansen/code/download-from-googledrive.R
@@ -17,48 +17,48 @@ download.from.googledrive <- function(
     require(googledrive);
 
     ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
-    DF.drive <- as.data.frame(googledrive::drive_find());
+    DF.googledrive.find <- as.data.frame(googledrive::drive_find());
 
     saveRDS(
-        object = DF.drive,
-        file   = "DF-google-drive.RData"
+        object =  DF.googledrive.find,
+        file   = "DF-googledrive-find.RData"
         );
 
-    # cat("\nstr(DF.drive)\n");
-    # print( str(DF.drive)   );
+    # cat("\nstr(DF.googledrive.find)\n");
+    # print( str(DF.googledrive.find)   );
     #
-    # cat("\nDF.drive[,c('name','id')]\n");
-    # print( DF.drive[,c('name','id')]   );
+    # cat("\nDF.googledrive.find[,c('name','id')]\n");
+    # print( DF.googledrive.find[,c('name','id')]   );
 
     ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
-    row.index <- which(DF.drive[,'name'] == google.drive.folder);
-    google.drive.ID <- DF.drive[row.index[1],'id'];
+    row.index <- which(DF.googledrive.find[,'name'] == google.drive.folder);
+    google.drive.ID <- DF.googledrive.find[row.index[1],'id'];
 
-    DF.earth.engine <- as.data.frame(googledrive::drive_ls(
+    DF.googledrive.ls.folder <- as.data.frame(googledrive::drive_ls(
         path    = googledrive::as_id(google.drive.ID),
         pattern = "."
         ));
 
     saveRDS(
-        object = DF.earth.engine,
-        file   = "DF-Drive-ls-folder.RData"
+        object =  DF.googledrive.ls.folder,
+        file   = "DF-googledrive-ls-folder.RData"
         );
 
-    # cat("\nDF.earth.engine\n");
-    # print( DF.earth.engine   );
+    # cat("\nDF.googledrive.ls.folder\n");
+    # print( DF.googledrive.ls.folder   );
 
     ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
     for ( temp.pattern in patterns ) {
         cat("\n### processing pattern:",temp.pattern,"\n");
         is.matched   <- grepl(
-            x           = DF.earth.engine[,'name'],
+            x           = DF.googledrive.ls.folder[,'name'],
             pattern     = temp.pattern,
             ignore.case = TRUE
             );
         n.is.matched <- sum(is.matched);
         cat("\n# ",n.is.matched,"object(s) found matching pattern:",temp.pattern,"\n")
         if ( sum(is.matched) > 0 ) {
-            DF.temp <- DF.earth.engine[is.matched,];
+            DF.temp <- DF.googledrive.ls.folder[is.matched,];
             for ( row.index in seq(1,nrow(DF.temp)) ) {
                 temp.id   <- DF.temp[row.index,'id'  ];
                 temp.name <- DF.temp[row.index,'name'];
