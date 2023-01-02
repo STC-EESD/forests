22ba50b9da08c9bba7e5840efa77306c698886b0
Kenneth Chu <kennethchu@Kenneths-MacBook-Pro.local>
2023-01-02 17:45:33 -0500

    updated: run-wget.sh stdout.sh.run-wget stderr.sh.run-wget; added: NFI_T4_FOR_AREA_SEP_en.csv NFI_T4_FOR_AREA_SEP_en.pdf NFI_T4_FOR_AREA_en.csv NFI_T4_FOR_AREA_en.pdf

--- 000-data/2023-01-02.01/run-wget.sh
+++ 000-data/2023-01-02.01/run-wget.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 
+# https://nfi.nfis.org/en/standardreports
+# Statistical Summaries for Terrestrial Ecozones
+# First Remeasurement Data Reports (2007--2017)
+
 TARGET_FILES=( \
+    # Land Cover: Area (1000 ha) of land cover by terrestrial ecozone in Canada
     "https://nfi.nfis.org/resources/general/summaries/t1/en/NFI/csv/NFI_T1_LC_AREA_en.csv" \
     "https://nfi.nfis.org/resources/general/summaries/t1/en/NFI/pdf/NFI_T1_LC_AREA_en.pdf" \
+    # Land Cover: Area (1000 ha) of land cover by terrestrial ecozone in Canada, relative standard error
     "https://nfi.nfis.org/resources/general/summaries/t1/en/NFI/csv/NFI_T1_LC_AREA_SEP_en.csv" \
     "https://nfi.nfis.org/resources/general/summaries/t1/en/NFI/pdf/NFI_T1_LC_AREA_SEP_en.pdf"
+    # Forest Area: Area (1000 ha) of forest and non-forest land by terrestrial ecozone in Canada
+    "https://nfi.nfis.org/resources/general/summaries/t1/en/NFI/csv/NFI_T4_FOR_AREA_en.csv" \
+    "https://nfi.nfis.org/resources/general/summaries/t1/en/NFI/pdf/NFI_T4_FOR_AREA_en.pdf" \
+    # Forest Area: Area (1000 ha) of forest and non-forest land by terrestrial ecozone in Canada, relative standard error
+    "https://nfi.nfis.org/resources/general/summaries/t1/en/NFI/csv/NFI_T4_FOR_AREA_SEP_en.csv" \
+    "https://nfi.nfis.org/resources/general/summaries/t1/en/NFI/pdf/NFI_T4_FOR_AREA_SEP_en.pdf"
     )
+
+echo
+echo
 
 for tempzip in "${TARGET_FILES[@]}"
 do
