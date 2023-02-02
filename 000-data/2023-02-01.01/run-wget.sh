b8951ef079a7e6395bbb843d3c50a111cc06e71c
Kenneth Chu <kennethchu@Kenneths-MacBook-Pro.local>
2023-02-01 21:24:16 -0500

    added: 000-data/2023-02-01.01/run-wget.sh

---
+++ 000-data/2023-02-01.01/run-wget.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+# https://ccea-ccae.org/ecozones-downloads/
+
+SHP_FILES=( \
+    # CA Forest Fires 1985-2020
+    "https://opendata.nfis.org/downloads/forest_change/CA_Forest_Fire_1985-2020.zip" \
+    # CA Forest Harvest 1985-2020
+    "https://opendata.nfis.org/downloads/forest_change/CA_Forest_Harvest_1985-2020.zip"
+    # #
+    # "" \
+    # #
+    # "" \
+    # #
+    # "" \
+    # #
+    # "" \
+    # #
+    # "" \
+    # #
+    # "" \
+    # #
+    # "" \
+    # #
+    # "" \
+    # #
+    # "" \
+    # #
+    # "" \
+    # #
+    # "" \
+    # #
+    # "" \
+    # #
+    # "" \
+    # #
+    # "" \
+    # #
+    # "" \
+    # #
+    # "" \
+    # #
+    # "" \
+    # #
+    # "" \
+    # #
+    # "" \
+    )
+
+for tempzip in "${SHP_FILES[@]}"
+do
+    echo downloading: ${tempzip}
+    wget ${tempzip}
+done
+
+echo
+
+for tempzip in *.zip; do
+    echo unzipping: ${tempzip}
+    tempstem=`basename ${tempzip} .zip`
+    unzip ${tempzip} -d ${tempstem}
+done
+
+echo
+echo done
+echo
